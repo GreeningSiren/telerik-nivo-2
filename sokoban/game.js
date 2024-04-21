fab33d53384f1f984542b4ed823359bee74ee734
GreeningSiren <greeningsiren@gmail.com>
2024-04-21 19:10:32 +0300

    sokoban 3

--- sokoban/game.js
+++ sokoban/game.js
@@ -4,7 +4,7 @@ const GRID_SIZE = 10
 maze = []
 for (let i = 0; i < GRID_SIZE; i++) {
     maze[i] = []
-    for(let j = 0; j < GRID_SIZE; j++) {
+    for (let j = 0; j < GRID_SIZE; j++) {
         maze[i][j] = 1 // wall
     }
 }
@@ -14,39 +14,32 @@ maze = [
     // 2 - kotiq
     // 3 - player
     // 4 - target
-    // 5 - target with bopx
-    [0,0,1,1,1,1,1,0,0,0],
-    [1,1,1,0,0,0,1,0,0,0],
-    [1,4,3,2,0,0,1,0,0,0],
-    [1,1,1,0,2,4,1,0,0,0],
-    [1,4,1,1,2,0,1,0,0,0],
-    [1,0,1,0,4,0,1,1,0,0],
-    [1,2,0,2,2,2,4,1,0,0],
-    [1,0,0,0,4,0,0,1,0,0],
-    [1,1,1,1,1,1,1,1,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
+    // 5 - target i kotiq
+    [0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+    [1, 1, 1, 0, 0, 0, 1, 0, 0, 0],
+    [1, 4, 0, 2, 0, 0, 1, 0, 0, 0],
+    [1, 1, 1, 0, 2, 4, 1, 0, 0, 0],
+    [1, 4, 1, 1, 2, 0, 1, 0, 0, 0],
+    [1, 0, 1, 0, 4, 0, 1, 1, 0, 0],
+    [1, 2, 0, 5, 2, 2, 4, 1, 0, 0],
+    [1, 0, 0, 0, 4, 0, 0, 1, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
-// maze = generateMaze(brKletki-2, brKletki-2);
+let target = tryToLoad("target", "red")
+let targetBox = tryToLoad("targetBox", "black")
 
 function init() {
-    target = tryToLoad("target","red")
-    targetBox = tryToLoad("targetBox","black")
     // Kodut tuk se izpulnqva vednuj v nachaloto
-    for(i = 0; i < GRID_SIZE; i++) {
-        for(j = 0; j < GRID_SIZE; j++) {
-            if(maze[i][j] === 3) {
-                myX = i;
-                myY = j;
-            }
-        }
-    }
+    myX = 2;
+    myY = 2;
     console.log(maze)
 }
 
 function update() {
     // Kodut tuk se izpulnqva (okolo) 100 puti v sekunda
-    myX = myX + (mouseX - myX) / 10;
-    myY = myY + (mouseY - myY) / 10;
+    // myX = myX + (mouseX - myX) / 10;
+    // myY = myY + (mouseY - myY) / 10;
 }
 
 function draw() {
@@ -56,20 +49,19 @@ function draw() {
     for (let i = 0; i < GRID_SIZE; i++) {
         for (let j = 0; j < GRID_SIZE; j++) {
             if (maze[j][i] === 1) { // Stena
-                drawImage(barrelGrey, i *50, j *50, 50, 50)
-            }else if(maze[j][i] === 3) { // Igrach
-                drawImage(femaleAction,i*50, j *50, 50, 50)
-            }else if(maze[j][i] === 0) { // poda
-                drawImage(paddle,i*50, j * 50, 50, 50)
-            }else if(maze[j][i] === 2){
-                drawImage(box,i*50, j *50, 50,50)
-            }else if(maze[j][i] === 4) { // target
-                drawImage(target,i*50,j*50,50,50)
-            }else if(maze[j][i] === 5) {// target s kotiq
-                drawImage(targetBox,i*50,j*50,50,50)
+                drawImage(barrelGrey, i * 50, j * 50, 50, 50)
+            } else if (maze[j][i] === 0) { // poda
+                drawImage(paddle, i * 50, j * 50, 50, 50)
+            } else if (maze[j][i] === 2) {
+                drawImage(box, i * 50, j * 50, 50, 50)
+            } else if (maze[j][i] === 4) { // target
+                drawImage(target, i * 50, j * 50, 50, 50)
+            } else if (maze[j][i] === 5) {// target s kotiq
+                drawImage(targetBox, i * 50, j * 50, 50, 50)
             }
         }
     }
+    drawImage(femaleAction, myX * 50, myY * 50, 50, 50);
 }
 
 function mouseup() {
@@ -80,16 +72,64 @@ function mouseup() {
 function keyup(key) {
     // Pechatai koda na natisnatiq klavish
     console.log("Pressed", key);
-    maze[myX][myY] = 0;
-    if(key === 87) {
-        myY--
-    }else if(key === 68) {
-        myX++
-    }else if(key === 83) {
-        myY++
-    }else if(key === 65) {
-        myX--
+    // maze[myY][myX] = 0;
+    if (key === 87) {
+        if (maze[myY - 1][myX] === 0 || maze[myY - 1][myX] === 4) {
+            myY--
+        } else if (maze[myY - 1][myX] === 2) {
+            if (maze[myY - 2][myX] === 0) {
+                maze[myY - 2][myX] = 2
+                maze[myY - 1][myX] = 0
+                myY--
+            } else if (maze[myY - 2][myX] === 4) {
+                maze[myY - 2][myX] = 5
+                maze[myY - 1][myX] = 0
+                myY--
+            }
+        }
+    } else if (key === 68) {
+        if (maze[myY][myX + 1] === 0 || maze[myY][myX + 1] === 4) {
+            myX++
+        } else if (maze[myY][myX + 1] === 2) {
+            if (maze[myY][myX + 2] === 0) {
+                maze[myY][myX + 2] = 2
+                maze[myY][myX + 1] = 0
+                myX++
+            } else if (maze[myY][myX + 2] === 4) {
+                maze[myY][myX + 2] = 5
+                maze[myY][myX + 1] = 0
+                myX++
+            }
+        }
+    } else if (key === 83) {
+        if (maze[myY + 1][myX] === 0 || maze[myY + 1][myX] === 4) {
+            myY++
+        } else if (maze[myY + 1][myX] === 2) {
+            if (maze[myY + 2][myX] === 0) {
+                maze[myY + 2][myX] = 2
+                maze[myY + 1][myX] = 0
+                myY++
+            } else if (maze[myY + 2][myX] === 4) {
+                maze[myY + 2][myX] = 5
+                maze[myY + 1][myX] = 0
+                myY++
+            }
+        }
+    } else if (key === 65) {
+        if (maze[myY][myX - 1] === 0 || maze[myY][myX - 1] === 4) {
+            myX--
+        } else if (maze[myY][myX - 1] === 2) {
+            if (maze[myY][myX - 2] === 0) {
+                maze[myY][myX - 2] = 2
+                maze[myY][myX - 1] = 0
+                myX--
+            } else if (maze[myY][myX - 2] === 4) {
+                maze[myY][myX - 2] = 5
+                maze[myY][myX - 1] = 0
+                myX--
+            }
+        }
     }
-    maze[myX][myY] = 5
+    // maze[myY][myX] = 3
 }
 
