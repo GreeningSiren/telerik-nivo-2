8e176e6e1a80d0c68819ce2bcb8a11779505b90e
GreeningSiren <greeningsiren@gmail.com>
2024-04-28 18:59:13 +0300

    womp womp ;(

--- leveleditor/game.js
+++ leveleditor/game.js
@@ -2,6 +2,7 @@ const GRID_SIZE = 10
 let myX, myY;
 let target = tryToLoad("target", "red");
 let targetBox = tryToLoad("targetBox", "black");
+let zaSlagane = 1
 
 function init() {
     // Kodut tuk se izpulnqva vednuj v nachaloto
@@ -10,8 +11,10 @@ function init() {
     maze = []
     for (let i = 0; i < GRID_SIZE; i++) {
         maze[i] = []
+        for (let j = 0; j < GRID_SIZE; j++) {
+            maze[i][j] = 0
+        }
     }
-    maze = JSON.parse('[[0,1,1,1,0,0,0,0,0,0],[1,0,0,0,0,0,3,0,0,0],[1,0,0,0,0,0,0,0,0,0],[1,0,1,1,0,0,0,0,0,0],[1,0,0,0,1,0,1,1,1,0],[0,1,1,1,0,1,0,0,0,1],[0,0,0,0,0,0,1,1,0,1],[0,0,0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0,0,1],[0,0,0,0,0,0,1,1,1,0]]')
 }
 
 function update() {
@@ -30,8 +33,8 @@ function draw() {
                 drawImage(paddle, i * 50, j * 50, 50, 50);
             } else if (maze[j][i] === 2) {
                 drawImage(box, i * 50, j * 50, 50, 50);
-            }else if(maze[j][i] === 3) {
-                drawImage(femaleAction,i*50,j*50,50,50)
+            } else if (maze[j][i] === 3) {
+                drawImage(femaleAction, i * 50, j * 50, 50, 50)
             } else if (maze[j][i] === 4) { // target
                 drawImage(target, i * 50, j * 50, 50, 50);
             } else if (maze[j][i] === 5) {// target s kotiq
@@ -43,20 +46,31 @@ function draw() {
 
 function mouseup() {
     // Pri klik s lqv buton - pokaji koordinatite na mishkata
-    if(mouseX < 500 && mouseY < 500) {
+    if (mouseX < 500 && mouseY < 500) {
         let cellX = Math.floor(mouseX / 50)
         let cellY = Math.floor(mouseY / 50)
-        console.log("Mouse clicked at", mouseX, mouseY, cellX, cellY);
-        if (maze[cellY][cellX] !== 5) {
-            maze[cellY][cellX] += 1
-        } else {
-            maze[cellY][cellX] = 0
+        console.log("Mouse clicked at", mouseX, mouseY, cellX, cellY,zaSlagane);
+        for(let i=0;i<GRID_SIZE;i++) {
+            for(let j=0;j<GRID_SIZE;j++) {
+                if(maze[i][j] === 3 && zaSlagane === 3) {
+                    maze[i][j] = 0
+                }
+            }
         }
+        maze[cellY][cellX] = zaSlagane
     }
 }
 
 function keyup(key) {
     // Pechatai koda na natisnatiq klavish
     console.log("Pressed", key);
+    if (key === 32) {
+        console.log("'" + JSON.stringify(maze) + "'")
+    }
+    for (let i = 0; i < 6; i++) {
+        if (key === i + 48) {
+            zaSlagane = i
+        }
+    }
 }
 
