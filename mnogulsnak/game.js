dc1fe8ef5114ba775450cbc35742ab5a3700a16d
GreeningSiren <greeningsiren@gmail.com>
2024-05-12 22:07:11 +0300

    Ne e gotovo ;(

--- mnogulsnak/game.js
+++ mnogulsnak/game.js
@@ -11,10 +11,11 @@ function drawMnogougulnik(x, y, radius, brStrani, style = "Black") {
     // context.fill();
     context.stroke();
 }
-function pythagorean(sideA, sideB){
+
+function pythagorean(sideA, sideB) {
     // Use the Pythagorean theorem to calculate the length of the hypotenuse.
     // return Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
-    return Math.hypot(sideA,sideB)
+    return Math.hypot(sideA, sideB)
 }
 
 // Suzdavame promenlivi
@@ -28,9 +29,9 @@ function init() {
     // Kodut tuk se izpulnqva vednuj v nachaloto
     myX = 300;
     myY = 300;
-    radius = 25;
+    radius = 12;
     x = radius;
-    y = canvas.height ;
+    y = canvas.height;
 }
 
 function update() {
@@ -41,22 +42,15 @@ function update() {
 
 function draw() {
     // Tuk naprogramirai kakvo da se risuva
-    // context.fillStyle = "black"
-    // context.fillRect(0, 0, GRID_HEIGHT, GRID_WIDTH)
-    for (let i = 0; i <30; i++) {
+    context.fillStyle = "#1c1c1c"
+    context.fillRect(0, 0, canvas.height, canvas.height)
+    for (let i = 0; i < 30; i++) {
         for (let j = 0; j < 30; j++) {
-            let visochina = pythagorean(radius / 2, (Math.sqrt(3 / 4)* radius))
-            let a = y - visochina - (j * (visochina - 2) * 2) - (i * visochina);
-
+            let visochina = pythagorean(radius / 2, (Math.sqrt(3 / 4) * radius))
             drawMnogougulnik(
-                (x + (3 / 2 * radius) *i),
-                (a),
-                radius, 6, "black")
-            // x += (3 / 2 * radius);
-            // console.log(x,y)
-            // y -= Math.hypot( radius/2,Math.sqrt(3/4)*radius)
-            //console.log(a);
-            //console.log(y);
+                (x + (3 / 2 * radius) * i),
+                (y - visochina - (j * (visochina - 2) * 2) - (i * visochina)),
+                radius, 6, "#f4f4f4")
         }
     }
 }
